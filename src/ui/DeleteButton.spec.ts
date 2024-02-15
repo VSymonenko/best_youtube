14225191dac8db5adddb48eeb7350bc8320af6d9
V.Symonenko <simonenko744@gmail.com>
2024-02-15 14:50:27 +0200

    test: stub math.random

--- src/ui/DeleteButton.spec.ts
+++ src/ui/DeleteButton.spec.ts
@@ -2,9 +2,14 @@
  * @vitest-environment jsdom
  */
 
-import { expect, test } from 'vitest';
+import { beforeEach, expect, test, vi } from 'vitest';
 
 import { CreateButton } from './DeleteButton';
+
+beforeEach(() => {
+  vi.spyOn(globalThis.Math, 'random').mockReturnValue(0.5);
+})
+
 
 test('should rendered correctly', () => {
   const button = CreateButton();
